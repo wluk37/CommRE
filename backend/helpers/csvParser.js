88451dabb0af7d8f7b396c2d517e985597088785
Wesley Luk <wluk37@gmail.com>
2021-11-12 10:48:15 -0800

    Remove console log

--- backend/helpers/csvParser.js
+++ backend/helpers/csvParser.js
@@ -4,13 +4,11 @@ const parser = parse({ columns: true });
 const csvFile = "./data/Sale-Data.csv";
 
 let csvData = [];
+
 fs.createReadStream(csvFile)
   .pipe(parser)
   .on("data", (csvRow) => {
     csvData.push(csvRow);
-  })
-  .on("end", () => {
-    // console.log(csvData);
   });
 
 module.exports = csvData;
