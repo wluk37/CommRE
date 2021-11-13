9142d4cf642812dcf941f4be94e92568d0c72a1a
Wesley Luk <wluk37@gmail.com>
2021-11-13 02:33:35 -0800

    Fix component alignment and add responsive design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import SalesTable from "./components/SalesTable";
 import React, { useState, useEffect, useRef } from "react";
+import PieChart from "./components/PieChart";
 import { fetchAgents, fetchSales, fetchTypes } from "./utils/fetcher";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 function App() {
   const [salesData, setSalesData] = useState({});
   const [agentList, setAgentList] = useState([]);
   const [agentSelected, setAgentSelected] = useState("");
-  const [pieData, setPieData] = useState({});
+  const [pieData, setPieData] = useState([]);
 
+  const theme = useTheme();
+  const screenSize = useMediaQuery(theme.breakpoints.up("sm"))
+    ? "row"
+    : "column";
+  const fontSize = useMediaQuery(theme.breakpoints.up("sm")) ? "2vw" : "7vw";
   const firstRender = useRef(true);
   useEffect(() => {
     if (firstRender.current) {
@@ -26,7 +34,12 @@ function App() {
   useEffect(() => {
     if (agentSelected !== undefined && agentSelected !== "") {
       const getPropertyTypes = async () => {
-        setPieData(await fetchTypes(agentSelected));
+        const propertyTypes = await fetchTypes(agentSelected);
+        const result = [];
+        for (const [key, value] of Object.entries(propertyTypes)) {
+          result.push([key, value]);
+        }
+        setPieData(result);
       };
       getPropertyTypes();
     }
@@ -34,14 +47,20 @@ function App() {
 
   return (
     <div className="App">
-      {
-        <SalesTable
-          agents={agentList}
-          selected={agentSelected}
-          sales={salesData}
-          onSelect={setAgentSelected}
-        />
-      }
+      <h1 style={{ fontSize: fontSize }}>Data Analysis for CommRE</h1>
+      <div className="Wrapper" style={{ flexDirection: screenSize }}>
+        <div className="TableWrapper">
+          {
+            <SalesTable
+              agents={agentList}
+              selected={agentSelected}
+              sales={salesData}
+              onSelect={setAgentSelected}
+            />
+          }
+        </div>
+        {<PieChart propertyTypes={pieData} agent={agentSelected} />}
+      </div>
     </div>
   );
 }
