68c6cdb3f30907dccfde949bce6e469d2c21bc8b
Wesley Luk <wluk37@gmail.com>
2021-11-13 00:43:28 -0800

    App fetches and renders agents and sales data to SalesTable component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,47 @@
 import "./App.css";
+import SalesTable from "./components/SalesTable";
+import React, { useState, useEffect, useRef } from "react";
+import { fetchAgents, fetchSales, fetchTypes } from "./utils/fetcher";
 
 function App() {
+  const [salesData, setSalesData] = useState({});
+  const [agentList, setAgentList] = useState([]);
+  const [agentSelected, setAgentSelected] = useState("");
+  const [pieData, setPieData] = useState({});
+
+  const firstRender = useRef(true);
+  useEffect(() => {
+    if (firstRender.current) {
+      const getSalesData = async () => {
+        setSalesData(await fetchSales());
+        setAgentList(await fetchAgents());
+      };
+      getSalesData();
+      firstRender.current = false;
+    } else {
+      setAgentSelected(agentList[0]);
+    }
+  }, [salesData, agentList]);
+
+  useEffect(() => {
+    if (agentSelected !== undefined && agentSelected !== "") {
+      const getPropertyTypes = async () => {
+        setPieData(await fetchTypes(agentSelected));
+      };
+      getPropertyTypes();
+    }
+  }, [agentSelected]);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      {
+        <SalesTable
+          agents={agentList}
+          selected={agentSelected}
+          sales={salesData}
+          onSelect={setAgentSelected}
+        />
+      }
     </div>
   );
 }
